3028734971efa0d7cfad3d0c7c45b147bcefe562
Nathaniel Bender <nsb2@calvin.edu>
2016-09-30 15:44:58 -0400

    Changed activity title for help page

--- lab04/MyApplication/app/src/main/java/nsb2/calvin/edu/HelpPage.java
+++ lab04/MyApplication/app/src/main/java/nsb2/calvin/edu/HelpPage.java
@@ -15,6 +15,7 @@ public class HelpPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help_page);
+        setTitle("Lab04 - About");
     }
 
 }
